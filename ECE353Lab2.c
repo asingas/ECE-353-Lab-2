4b7cfffd657f08e5ebf6624561d1d2fe17f04485
Arthur Singas <40192855+asingas@users.noreply.github.com>
2018-12-16 22:04:30 -0500

    Fixed indents/cleaned up code

--- ECE353Lab2.c
+++ ECE353Lab2.c
@@ -1,5 +1,5 @@
-// ECE 353 - Lab 2
-// Written by: Arthur Singas, James Gnall, and Fedor Panikov
+// General MIDI Explorer
+// Written by: Arthur Singas
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -16,7 +16,7 @@
 #define BAUD_PRESCALE 7
 #define CPU_FREQ 4000000
 
-struct Note                                            // Struct to input and output Usart values
+struct Note                                            // Struct to input and output USART values
 {
 	unsigned short status;
 	unsigned short note_value;
@@ -70,51 +70,50 @@ void USART_Init()
 
 void USART_Transmit(unsigned char data)
 {
-	while ( !(UCSRA & (1 << UDRE)) );					// Wait for empty transmit buffer
+	while (!(UCSRA & (1 << UDRE)));					    // Wait for empty transmit buffer
 		UDR = data;										// Put data into buffer, sends the data
 }
 
 unsigned char USART_Receive(void)                       // return byte recieved from MIDI Ox
 {
-	while (!(UCSRA & (1 << RXC)) && (PINA &(1<<PA0)));
+	while (!(UCSRA & (1 << RXC)) && (PINA &(1 << PA0)));
 
-		if(!(PINA&(1<<PA0)))                             
+		if(!(PINA&(1 << PA0)))
+		{                             
 			return 0;
+		}
 	   	return UDR;
 }
 
 void USART_Flush(void)
 {
 	unsigned char dummy;
-	while (UCSRA & (1<<RXC));
+	while (UCSRA & (1 << RXC));
 		dummy = UDR;
 }
 //////////////////////////////
-// Analog to Digital /////////
+///// Analog to Digital //////
 //////////////////////////////
 
-void initADC(){
+void initADC()
+{
 	DDRA = DDRA & 0x7F;
 	ADMUX = (1<<REFS0);
 	ADCSRA = (1<< ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-	}
+}
 
 uint16_t readADC(uint8_t chan)
 {
 	ADMUX |= chan;
-
 	ADCSRA |=(1<< ADSC);    							// Start conversion
-          				
-	
-	while(!(ADCSRA &(1<<ADIF)));
-
+    while (!(ADCSRA &(1<<ADIF)));
 	ADCSRA |= (1<<ADIF);
 	return (ADC);
 
 }
 
 //////////////////////////////
-// EEPROM Implementation /////
+/// EEPROM Implementation  ///
 //////////////////////////////
 
 void write_eeprom_byte(unsigned short address, unsigned char data)
@@ -129,7 +128,7 @@ void write_eeprom_byte(unsigned short address, unsigned char data)
 
 unsigned char read_eeprom_byte(unsigned short address)
 {
-	while(EECR & (1<<EEWE));							// Waiting for previous write to complete
+	while (EECR & (1<<EEWE));							// Waiting for previous write to complete
 	EEAR = address;						
 	EECR |= (1<<EERE);									// Start eeprom read by writing EERE
 	EECR |= (1<<2);
@@ -137,17 +136,17 @@ unsigned char read_eeprom_byte(unsigned short address)
 	return EEDR;					   					// return data from data register
 }
 
-//////////////////////
-// RECORD Mode ///////
-//////////////////////
+//////////////////////////
+////// RECORD Mode ///////
+//////////////////////////
 
 void record_mode()
 {		 
-	int delayOff = 0;
 	char delayOnUpper = 0;
 	char delayOffUpper = 0;
 	char delayOnLower = 0;
 	char delayOffLower = 0;
+	int delayOff = 0;
 	int address = 0;
 	int visited = 0;
 	int validAddTop;
@@ -157,44 +156,45 @@ void record_mode()
 	int vBitBot = read_eeprom_byte(1019);
 	int valid = 0;
 	valid |= vBitBot;
-	valid = valid|(vBitTop<<8);
+	valid = valid|(vBitTop << 8);
 
 	timer1_init();
 
-
-
-	while(1)
+	while (1)
 	{
-		if(address >= 0x3F0)
+		if (address >= 0x3F0){
 			return;
+		}
 
 		DDRB = 0xFF;
 
-
-    	if(!(PINA &(1<<PA0)))
+    	if (!(PINA &(1<<PA0))){
 			break;
+    	}
 
 		USART_Init();
 		
-		if(visited == 1){
+		if (visited == 1){
 			address = 0;
 		}
 		
-		note_instance.status = USART_Receive();															// Receiving note byte values and storing them in note structure
+		note_instance.status = USART_Receive();									// Receiving note byte values and storing them in note structure
 		note_instance.note_value = USART_Receive();
 		note_instance.velocity = USART_Receive();
 		note_instance.time = TIM16_ReadTCNT1();
 		
 	
-		if (note_instance.status == 0x90 && note_instance.velocity == 0x40)
+		if (note_instance.status == 0x90 && note_instance.velocity == 0x40){
 			return;
+		}
 
-		if(note_instance.velocity == 0 || note_instance.status == 0 || note_instance.note_value == 0)
+		if (note_instance.velocity == 0 || note_instance.status == 0 || note_instance.note_value == 0){
 			return;
-		else{
-			
-			if(!(PINA &(1<<PA0)))
+		}
+		else {
+			if (!(PINA &(1<<PA0))){
 				return;
+			}
 
 			delayOnLower = (note_instance.time& 0xFF);
 			delayOnUpper = (note_instance.time>>8);
@@ -251,18 +251,19 @@ void playback_mode()
 	int time2;
 	int address = 0;
 
-	while(1){
+	while (1){
 		
-		if(address+10 <= valid){
+		if (address+10 <= valid){
 			note_instance.status = read_eeprom_byte(address);								
-			note_instance.note_value = read_eeprom_byte(address +1);
+			note_instance.note_value = read_eeprom_byte(address + 1);
 			note_instance.velocity = read_eeprom_byte(address + 2);
 			delayOnUpper = read_eeprom_byte(address + 3);
 			delayOnLower = read_eeprom_byte(address + 4);
 			address = address + 5;
 			
-			if(note_instance.note_value == 0)
+			if (note_instance.note_value == 0){
 				return;
+			}
 			
 			time1 = delayOnLower;
 			time1 = time1|(delayOnUpper<<8);
@@ -277,38 +278,40 @@ void playback_mode()
 			TCNT1 =0;
 			
 			note_instance.status = read_eeprom_byte(address);
-			note_instance.note_value = read_eeprom_byte(address +1);
+			note_instance.note_value = read_eeprom_byte(address + 1);
 			note_instance.velocity = read_eeprom_byte(address + 2);
 			delayOffUpper = read_eeprom_byte(address + 3);
 			delayOffLower = read_eeprom_byte(address + 4);
 			address = address + 5;
 
-			if(note_instance.note_value == 0)
+			if (note_instance.note_value == 0){
 				return;
+			}
 			
 			time2 = delayOffLower;
 			time2 = time2|(delayOffUpper<<8);
 			time2 = (time2/2232) * 500;
 			
-				_delay_ms(time2);
+			_delay_ms(time2);
 		
 			PORTB = note_instance.note_value;
 			USART_Transmit(note_instance.status);
 			USART_Transmit(note_instance.note_value);
 			USART_Transmit(note_instance.velocity);
-
 			_delay_ms(500);
-		}
-		else
+			
+		} else {
 			address = 0;
+		}
 
 		DDRB = 0xFF;
 
-
-		if((PINA&(1<<PA2)) && (PINA & (1<<PA1)))
+		if((PINA&(1<<PA2)) && (PINA & (1<<PA1))){
 			modify_mode();
-		else if(!(PINA & (1<<PA1)))
+		}
+		else if(!(PINA & (1<<PA1))){
 			break;
+		}
 	}
 }
 
@@ -335,26 +338,20 @@ void modify_mode()
 
 	while(1){
 		
-		if(address+10 <= valid){
-		
-	
+		if (address+10 <= valid){
 			uint16_t volt = 0;
             volt = readADC(7);
 
-
-			if(volt > 700){  
-												// Covered, slow down
-				timeChange = 150;
+			if (volt > 700){  
+				timeChange = 150;				// Covered, slow down
 			}
-			else if(volt < 100){   					
+			else if (volt < 100){   					
 				timeChange = 10;				// Lit up, speed up
 			}
-			else if(volt > 185){
+			else if (volt > 185){
 				timeChange = 1;					// Else, normal
 			}
 
-
-			
 			note_instance.status = read_eeprom_byte(address);
 			note_instance.note_value = read_eeprom_byte(address +1);
 			note_instance.velocity = read_eeprom_byte(address + 2);
@@ -362,22 +359,23 @@ void modify_mode()
 			delayOnLower = read_eeprom_byte(address + 4);
 			address = address + 5;
 			
-			if(note_instance.note_value == 0)
+			if (note_instance.note_value == 0){
 				return;
+			}
 			
 			time1 = delayOnLower;
 			time1 = time1|(delayOnUpper<<8);
 			time1 = (time1/2232) * 500;
 
-			if(timeChange == 150)
+			if (timeChange == 150){
 				time1 = time1*4;
-			if(timeChange == 10)
+			}
+			if (timeChange == 10){
 				time1 = time1/timeChange;
-			if(timeChange == 1)
+			}
+			if (timeChange == 1){
 				time1 = time1/timeChange;
-
-			
-
+			}
 			_delay_ms(time1);
 
 			//display and send note to USART
@@ -393,40 +391,40 @@ void modify_mode()
 			delayOffLower = read_eeprom_byte(address + 4);
 			address = address + 5;
 
-			if(note_instance.note_value == 0)
+			if (note_instance.note_value == 0){
 				return;
+			}
 			
 			time2 = delayOffLower;
 			time2 = time2|(delayOffUpper<<8);
 			time2 = (time2/2232) * 500;
 
-			if(timeChange == 150)
+			if (timeChange == 150){
 				time2 = time2*4;
-			if(timeChange == 10)
+			}
+			if (timeChange == 10){
 				time2 = time2/timeChange;
-			if(timeChange == 1)
+			}
+			if (timeChange == 1){
 				time2 = time2;
-
+			}
 
 			_delay_ms(time2);
-			
-			
-			
 			PORTB = note_instance.note_value;
 			USART_Transmit(note_instance.status);
 			USART_Transmit(note_instance.note_value);
 			USART_Transmit(note_instance.velocity);
-
 			_delay_ms(500);
 		}
-		else
+		else {
 			address = 0;
+		}
 
 		DDRB = 0xFF;
 
-
-		if (!((PINA&(1<<PA2)) && (PINA & (1<<PA1))))
+		if (!((PINA&(1<<PA2)) && (PINA & (1<<PA1)))){
 			return;
+		}
 	}
 }
 
@@ -442,21 +440,23 @@ int main (void)
 	while(1)
 	{
 		DDRB = 0xFF;
-        DDRA = DDRA &0xF8;    					// Record
+        DDRA = DDRA &0xF8;    										    // Record
 
 		// Choosing which mode 
 
-        if (PINA &(1<<PA0))         	 			// if record, go to record mode
+        if (PINA &(1 << PA0)){      	 								// if record, go to record mode
 	        record_mode();	
-     	else{                          				// if not in record, modify or playback
-      		if((PINA&(1<<PA2)) && (PINA & (1<<PA1)))// modify mode
+	    } else{                          							    // if not in record, modify or playback
+      		if((PINA&(1 << PA2)) && (PINA & (1 << PA1))){				// modify mode
 				modify_mode();
-			else if(PINA & (1<<PA1)) 				// if not modify or record, playback		   	    
+			}
+			else if(PINA & (1 << PA1)){ 								// if not modify or record, playback		   	    
 			    playback_mode();
-		    else{
-			    PORTB = 0x00;               	 			// if none on, continue
+			} else {
+			    PORTB = 0x00;               	 					    // if none on, continue
 				continue;
 			}
 		}
 	}	 
 }
+
